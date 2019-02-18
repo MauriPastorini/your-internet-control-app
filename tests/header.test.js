ef490d0f2d4ae6473a7414f5666a76cbfed4cc7c
Mauricio Pastorini <mauri295@gmail.com>
2019-02-18 14:35:04 -0300

    Travis config

---
+++ tests/header.test.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+
+let browser, page;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox']
+    });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+});
+
+afterEach(async () => {
+    await browser.close();
+})
+
+test('We can launch a browser', async () => {
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    expect(text).toEqual('Blogster');
+})
